3e2608881d6cfdf215f78a89faa79f4339ed990c
Josh-Cruz <josh@digitalmonestary.com>
2018-10-24 09:36:13 -0700

    got my mongoose APi working

---
+++ mean-assignment-landscape/04-mongodb/05-api/02-restful-task-api/server/config/routes.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const tasks = require('../controllers/tasks');
+
+User = mongoose.model('Task');
+
+module.exports = function(app) {
+  // // Root Request
+  // app.get('/', function(req, res) {
+  //   tasks.index(req, res);
+  // });
+
+  // // Add User Request
+  // app.post('/process', function(req, res) {
+  //   tasks.createOrUpdate(req, res);
+  // });
+
+  // //Display Content
+  // app.get('/wall', function(req, res) {
+  //   tasks.display(req, res);
+  // });
+
+  // Shows All Tasks
+  app.get('/tasks', tasks.index);
+
+  //Adds a New Task
+  app.post('/tasks', tasks.create);
+
+  //Find one task with id
+  app.get('/tasks/:id', tasks.show);
+
+  //Find and update one task with id
+  app.put('/tasks/:id', tasks.update);
+
+  //Find and delete one task with id
+  app.delete('/tasks/:id', tasks.delete );
+
+};
